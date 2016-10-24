5750eecde33ca12d2d6bcc54d467a8e944e6c8ed
dmikov <dmikov@gmail.com>
2016-10-24 15:54:03 -0400

    Reply functionality.

--- office365api/mail/inbox.py
+++ office365api/mail/inbox.py
@@ -1,8 +1,24 @@
 from office365api.mail.folder import Folder
+from office365api.model import Message
 
 
 class Inbox(Folder):
 
+    REPLY_URL = super().BASE_URL+'messages/{id}/reply'
+    REPLY_ALL_URL = super().BASE_URL+'messages/{id}/replyall'
+
     @property
     def folder_name(self):
         return 'Inbox'
+
+    def reply(self, message: Message, comment: str=None, to_all: bool=False):
+        """
+        Sends reply to sender and other recipients.
+        :param message: Message to reply to, only Id is important.
+        :param comment: Optional comment.
+        :param to_all: If true reply to other recipients as well.
+        :return: None
+        """
+        url = (self.REPLY_ALL_URL if to_all else self.REPLY_URL).format(id=message.Id)
+        self.connection.post(url=url, data={'Comment': comment or ''})
+
